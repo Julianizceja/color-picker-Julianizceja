80e0058e01b5f834ee80daeee68a18f725ec6b29
Julianizceja <julianizceja@csu.fullerton.edu>
2023-10-15 23:15:43 -0700

    Project

--- app/src/main/java/edu/jc/csu411/project1/MainActivity.kt
+++ app/src/main/java/edu/jc/csu411/project1/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.Switch
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +30,21 @@ class MainActivity : AppCompatActivity() {
     private var greenIntensity = 0.0
     private var blueIntensity = 0.0
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putDouble("redIntensity", redIntensity)
+        outState.putDouble("greenIntensity", greenIntensity)
+        outState.putDouble("blueIntensity", blueIntensity)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        redIntensity = savedInstanceState.getDouble("redIntensity", 0.0)
+        greenIntensity = savedInstanceState.getDouble("greenIntensity", 0.0)
+        blueIntensity = savedInstanceState.getDouble("blueIntensity", 0.0)
+        updatePumpkinColor()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,11 +59,18 @@ class MainActivity : AppCompatActivity() {
         seekBarBlue = findViewById(R.id.seekBarBlue)
         editTextBlue = findViewById(R.id.editTextBlue)
         imageColor = findViewById(R.id.imageColor)
-        //resetButton = findViewById(R.id.resetButton)
 
+        switchRed.isChecked = false
+        seekBarRed.isEnabled = false
+        editTextRed.isEnabled = false
+        switchGreen.isChecked = false
+        seekBarGreen.isEnabled = false
+        editTextGreen.isEnabled = false
+        switchBlue.isChecked = false
+        seekBarBlue.isEnabled = false
+        editTextBlue.isEnabled = false
 
-
-        updateColorBox()
+        updatePumpkinColor()
 
         switchRed.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -55,10 +78,12 @@ class MainActivity : AppCompatActivity() {
                 editTextRed.isEnabled = true
             } else {
                 redIntensity = 0.0
+                seekBarRed.progress = 0
                 seekBarRed.isEnabled = false
                 editTextRed.isEnabled = false
+                editTextRed.setText("0.0")
             }
-            updateColorBox()
+            updatePumpkinColor()
         }
 
         seekBarRed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -68,12 +93,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 redIntensity = progress.toDouble() / 100
                 editTextRed.setText(redIntensity.toString())
-                updateColorBox()
+                updatePumpkinColor()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        editTextRed.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = (s.toString().toDoubleOrNull() ?: 0.0).coerceIn(0.0, 1.0)
+                seekBarRed.progress = (inputVal * seekBarRed.max).roundToInt()
+                updatePumpkinColor()
+            }
         })
 
         switchGreen.setOnCheckedChangeListener { _, isChecked ->
@@ -83,9 +120,11 @@ class MainActivity : AppCompatActivity() {
             } else {
                 greenIntensity = 0.0
                 seekBarGreen.isEnabled = false
+                seekBarGreen.progress = 0
                 editTextGreen.isEnabled = false
+                editTextGreen.setText("0.0")
             }
-            updateColorBox()
+            updatePumpkinColor()
         }
 
         seekBarGreen.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -95,12 +134,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 greenIntensity = progress.toDouble() / 100
                 editTextGreen.setText(greenIntensity.toString())
-                updateColorBox()
+                updatePumpkinColor()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        editTextGreen.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = (s.toString().toDoubleOrNull() ?: 0.0).coerceIn(0.0, 1.0)
+                seekBarGreen.progress = (inputVal * seekBarGreen.max).roundToInt()
+                updatePumpkinColor()
+            }
         })
 
         switchBlue.setOnCheckedChangeListener { _, isChecked ->
@@ -110,9 +161,11 @@ class MainActivity : AppCompatActivity() {
             } else {
                 blueIntensity = 0.0
                 seekBarBlue.isEnabled = false
+                seekBarBlue.progress = 0
                 editTextBlue.isEnabled = false
+                editTextBlue.setText("0.0")
             }
-            updateColorBox()
+            updatePumpkinColor()
         }
 
         seekBarBlue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -122,12 +175,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 blueIntensity = progress.toDouble() / 100
                 editTextBlue.setText(blueIntensity.toString())
-                updateColorBox()
+                updatePumpkinColor()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        editTextBlue.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val inputVal = (s.toString().toDoubleOrNull() ?: 0.0).coerceIn(0.0, 1.0)
+                seekBarBlue.progress = (inputVal * seekBarBlue.max).roundToInt()
+                updatePumpkinColor()
+            }
         })
 
         val resetButton = findViewById<Button>(R.id.resetButton)
@@ -141,18 +206,34 @@ class MainActivity : AppCompatActivity() {
             switchBlue.isChecked = false
             seekBarBlue.progress = 0
             editTextBlue.setText("0.0")
-            updateColorBox()
+            updatePumpkinColor()
         }
-        //updateColorBox()
     }
 
-    private fun updateColorBox() {
-        val color = android.graphics.Color.rgb(
+    private fun updatePumpkinColor() {
+        val redIntensity = calculateIntensity(seekBarRed, editTextRed, switchRed)
+        val greenIntensity = calculateIntensity(seekBarGreen, editTextGreen, switchGreen)
+        val blueIntensity = calculateIntensity(seekBarBlue, editTextBlue, switchBlue)
 
+        val color = android.graphics.Color.rgb(
             (redIntensity * 255).toInt(),
             (greenIntensity * 255).toInt(),
             (blueIntensity * 255).toInt()
         )
         imageColor.setColorFilter(color)
+    }
+
+    private fun calculateIntensity(seekBar: SeekBar, editText: EditText, switch: Switch): Float {
+        return if (switch.isChecked) {
+            val seekBarVal = seekBar.progress / seekBar.max.toFloat()
+            val inputVal = editText.text.toString().toDoubleOrNull()?.toFloat() ?: 0.0f
+            if (seekBarVal > inputVal) {
+                seekBarVal
+            } else {
+                inputVal
+            }
+        } else {
+            0.0f
+        }
     }
 }
